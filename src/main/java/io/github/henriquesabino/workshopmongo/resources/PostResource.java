178490b855a5126588dce10424fcf214901aee5a
HenriqueSabino <h.sabinocosta@gmail.com>
2019-05-04 20:19:22 -0300

    Simple search with query methods

--- src/main/java/io/github/henriquesabino/workshopmongo/resources/PostResource.java
+++ src/main/java/io/github/henriquesabino/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package io.github.henriquesabino.workshopmongo.resources;
 
 import io.github.henriquesabino.workshopmongo.domain.Post;
+import io.github.henriquesabino.workshopmongo.resources.util.URL;
 import io.github.henriquesabino.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+    
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        
+        return ResponseEntity.ok().body(list);
     }
 }
